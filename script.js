461844d6197163f5487dd5e8c65c5708525c4199
Lucy Laurentino <9998329@BCS.NET.NZ>
2017-09-04 11:45:03 +1200

    finish if/else

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-let num1 = prompt("Please type in number 1", Number1)
-alert(num1)
-let num2 = prompt("Please type in number 2", Number2)
-alert(num2)
+
+let menu = prompt("This program will check to numbers and see which one is greater", "Menu")
+let num1 = 0
+let num2 = 0
+
+let answer1 = prompt("Please type in number1", "00")
+num1 += parseInt(answer1)
+let answer2 = prompt("Please type in number2", "00")
+num2 += parseInt(answer2)
 
 if(num2 > num1)
     {
-        console.log(num2 + "is greater than"+ num1)
+
+        console.log(num2  + " is greater than " +  num1)
     }
     else
     {
             if(num2 = num1)
                 {
-                    console.log(num2 + "is equal than"+ num1)
+                    console.log(num2  + " is equal than " +  num1)
                 }
                 else
                 {
-                    console.log(num2 + "is smaller than"+ num1)
+                    console.log(num2  + " is smaller than " + num1)
                 }
     }
